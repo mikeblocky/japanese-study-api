2bc728ee84c1b8e40ab9c488a556c0f0276844ef
Minh Nhat Le <151120107+mikeblocky@users.noreply.github.com>
2026-01-19 11:11:06 +0700

    Substantial improvements

--- src/main/java/com/japanesestudy/app/repository/CourseRepository.java
+++ src/main/java/com/japanesestudy/app/repository/CourseRepository.java
@@ -6,13 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
     List<Course> findByTitle(String title);
     List<Course> findByTitleAndOwner_Id(String title, Long ownerId);
-    Optional<Course> findByTitleAndOwnerId(String title, Long ownerId);
     
     @Query("SELECT c FROM Course c WHERE c.owner.id = ?1")
     List<Course> findByOwnerId(Long ownerId);
